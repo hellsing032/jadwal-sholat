f75835b1e4366e9fcf420c55efa245be23079695
hellsing032 <saidina.065121083@unpak.ac.id>
2024-02-07 16:49:30 +0700

    feat: permission geolocation

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+function success(position) {
+  console.log(position);
+}
+
+function error() {
+  alert("Posisi ente tidak dapat di akses");
+}
+
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert(
+      "Geolocation tidak di dukung di dalam browser ente, coba silahkan dengan browse lain"
+    );
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Jadwal Sholat";
 
   app.appendChild(h3);
+  userLocation();
 }
 
 index();
